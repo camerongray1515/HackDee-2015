9459dc1e52a0657fa4a396aa9e86511282fc53dc
Allan Brown <allanbrown239@gmail.com>
2015-04-04 21:16:24 +0100

    Pulling videos. Not finished.

--- static/js/application.js
+++ static/js/application.js
@@ -18,3 +18,26 @@ function add_playlist(){
 		}
 	});
 }
+
+function pull_videos(){
+	var array = new Array({"playlist_id": "aLMU_84BLLM",
+			  		       "slug": "aLMU_84BLLM",
+			  		       "thumbnail_url": "http://img.photobucket.com/albums/v233/thelittleredone/neds.jpg",
+			  		       "title": "DJ BAD BOI YA BAS 2K15"});
+
+	for (var i = 0; i < array.length; i++){
+		// var table = document.getElementsByTagName("tbody").item(1);
+		// var row = document.createElement("tr");
+		// var number = document.createElement("td");
+		// var name = document.createElement("td");
+		// var number_text = document.createTextNode(i.toString());
+		// var name_text = array[i]["title"];
+		// number.appendChild(number_text);
+		// name.appendChild(name_text);
+		// row.appendChild(number);
+		// row.appendChild(name);
+		// table.appendChild(row);
+
+		$("#tab_logic").append('<tr><td>' + )
+	}
+}
