98124343ee94c5e4d8445375e27837c454239f51
Kimberley McCann <kimberleylmccann@gmail.com>
2015-04-05 00:11:02 +0100

    apparently I've made changes...

--- static/js/voting.js
+++ static/js/voting.js
@@ -1,19 +1,20 @@
 $(document).ready(function(){
-    $("#thumbUp").click(voteUp())
-    $("#thumbDown").click(voteDown())
-    var video.rank = 0;
+    $("#thumbUp").click(voteUp(up_down))
+    $("#thumbDown").click(voteDown(up_down))
 });
 
-function voteUp(){
+function voteUp(up_down){
   $.post("/api/vote/",
   {
     up_down="up"
+    console.log(data["up_down"]);
   }
 }
 
-function voteDown(){
+function voteDown(up_down){
   $.post("/api/vote/",
   {
     up_down="down"
+    console.log(data["up_down"]);
   }
 }
